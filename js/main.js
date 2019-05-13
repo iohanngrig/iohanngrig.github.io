2d302fa250a0c281d8486047b995e236531cd8d6
Hovhannes Grigoryan <iohanngrig@gmail.com>
2019-05-12 23:59:08 -0400

    updated P3/P5, main.js, data added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 $(".mynavbar").load('mynavbar.html');
 
-// $(document).ready(function() {
-//     $('.navbar-bar > li > a[href^="' + location.pathname.split("/")[3] + '"]').parent().addClass('active');
-//     console.log(location.pathname.split("/")[3]);
-// });
-
 
 function appendData(data) {
     let mainContainer = document.getElementById("myData");
@@ -28,7 +23,7 @@ function appendData2(data) {
         let div = document.createElement("div");
         div.className = "row";
         div.innerHTML =  "<div class='col-md-7'>"
-                        +  "<a href=" + data[i].url + "><img alt=' ' class='img-fluid rounded mb-3 mb-md-0' src=" + data[i].media2 + "></a>"
+                        +   addMedia(data[i].url, data[i].media2)
                         +"</div>"
                         +"<div class='col-md-5'>"
                         +   "<h3>" + data[i].title + "</h3>"
@@ -43,24 +38,47 @@ function appendData2(data) {
     }
 }
 
+
 function appendData3(data) {
     let mainContainer = document.getElementById("myData3");
     for (let i = 0; i < data.length; i++) {
         let div = document.createElement("div");
         div.className = "row";
         div.innerHTML =  "<div class='col-md-7'>"
-                        +  "<a href=" + data[i].url + "><img alt=' ' class='img-fluid rounded mb-3 mb-md-0' "
-                        +                                   "src=" + data[i].media + "></a>"
+                        +   addMedia(data[i].url, data[i].media)
                         +"</div>"
                         +"<div class='col-md-5'>"
-                        +   "<h3>" + data[i].title + "</h3>"
-                        +   "<p>" + data[i].description + "</p>"
-                        +   "<a class='btn btn-primary' href=" + data[i].url + ">View Project"
-                        +     "<span class='glyphicon glyphicon-chevron-right'></span>"
-                        +   "</a>"
+                        +  "<h3>" + data[i].title + "</h3>"
+                        +  "<p>" + data[i].description + "</p>"
+                        +  "<a class='btn btn-primary' href=" + data[i].url + " target='_blank'>View Project"
+                        +    "<span class='glyphicon glyphicon-chevron-right'></span>"
+                        +  "</a>"
                         +"</div>"
                         +"<hr>";
         mainContainer.appendChild(div);
         mainContainer.append(document.createElement("hr"))
     }
 }
+
+
+function addMedia(url, filename) {
+  let lastChar = filename.charAt(filename.length - 1);
+  let output = "";
+  if (lastChar === '4') {
+      output = "<video width='630' height='315' controls='controls'>"
+              +  "<source src=" + filename + " type='video/mp4'>"
+              +"</video>";
+  }
+  else {
+      output = "<a href=" + url + " target='_blank'>"
+              +  "<img alt='' class='img-fluid rounded mb-3 mb-md-0' src=" + filename + ">"
+              +"</a>";
+  }
+  return output
+}
+
+
+// $(document).ready(function() {
+//     $('.navbar-bar > li > a[href^="' + location.pathname.split("/")[3] + '"]').parent().addClass('active');
+//     console.log(location.pathname.split("/")[3]);
+// });
